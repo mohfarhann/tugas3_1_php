b541c6bdec358eccf7e0c35db61ccf6ea364f16a
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:04:20 +0700

    Add files via upload

---
+++ tugas3_1.php
@@ -0,0 +1,12 @@
+<?php  
+	$s = 360;
+	$v = 90;
+	$t =  $s / $v;
+	$j = $t * 60;	
+
+	echo "<h5>Jarak kota A-Z = $s\KM </h5>";
+	echo "<h5>Kecepatan Motor = $v\Jam</h5>";
+	echo "<h5>Waktu Tempuh = $t Jam</h5>";
+	echo "<h5>Jawaban = $j Menit</h5>";
+
+?>
